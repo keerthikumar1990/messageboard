90e8a310bc1810d37c592e49bfd5fb399a8a04c2
keerthikumar <rkeerthikuimar1990@gmail.com>
2017-07-20 23:49:40 -0500

    Changed server.js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Message = require('./models/message');
-var User = require('./models/user');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var Message = mongoose.model("Message", {
+    msg: String
+});
 
+var User = mongoose.model("User", {
+    email: String,
+    pwd: String
+});
 
 app.use(bodyParser.json());
 app.use(function(req, res, next){
@@ -23,15 +30,22 @@ app.post('/api/message',function(req, res){
 });
 app.post('/auth/register',function(req, res){
     console.log(req.body);
-    var user = new User(req.body);
-    user.save(function(err, result){
-        if(err){
-            res.status(500).send({
-                message: err.message
+    User.findOne({email: req.body.email}, function(err, existingUser){
+        if(existingUser){
+            res.status(409).send({message: "Email already registered"})
+        }else{
+            var user = new User(req.body);
+            user.save(function(err, result){
+                if(err){
+                    res.status(500).send({
+                        message: err.message
+                    });
+                }
+                res.status(200).send({token: createToken(result)});
             });
         }
-        res.status(200);
     });
+    
 });
 function GetMessage(req, res){
     Message.find({}).exec(function(err, result){
@@ -47,3 +61,21 @@ mongoose.connect("mongodb://localhost:27017/test",function(err, db){
 var server = app.listen(5000, function(){
      console.log(`listening on port ${server.address().port}`);
 });
+
+function createToken(user){
+    var payload = {
+        sub: user._id,
+        iat: moment().unix(),
+        exp: moment().add(14, 'days').unix()
+    };
+    return jwt.encode(payload, 'secret');
+}
+
+function checkAuthenticated(req, res, next){
+    if(!req.header('Authorization')){
+        return res.status(401).send({message: "Please make sure your request has an authorization header"});
+    }
+    var token = req.header('Authorization').split('')[1];
+    var payload = jwt.decode(token,'secret');
+    
+}
